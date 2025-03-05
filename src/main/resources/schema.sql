0fa882e3c5a20cc954e4e76f8a1f1770e9e07188
남궁욱 <gunguk.nam@emro.co.kr>
2025-03-05 18:07:34 +0900

    dic temp

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,22 @@ CREATE TABLE IF NOT EXISTS sheet_data (
                                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                      data CLOB NOT NULL
 );
+
+-- CREATE TABLE IF NOT EXISTS MULTLANG (
+-- 	                                    MULTLANG_CCD VARCHAR2(50) NOT NULL,
+-- 										MULTLANG_KEY VARCHAR2(500) NOT NULL,
+-- 										MULTLANG_TRANSL_CONT VARCHAR2(500),
+-- 										MULTLANG_MOD_DTTM TIMESTAMP,
+-- 										MULTLANG_TRANSL_CONT_ABBR VARCHAR2(100),
+-- 										MULTLANG_ABBR_USE_YN CHAR(1),
+-- 										MULTLANG_TYP VARCHAR2(100) NOT NULL,
+-- 										RMK VARCHAR2(1000),
+-- 										STS CHAR(1) DEFAULT 'C',
+-- 										REGR_ID VARCHAR2(50),
+-- 										REG_DTTM TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+-- 										MODR_ID VARCHAR2(50),
+-- 										MOD_DTTM TIMESTAMP,
+-- 										MULTLANG_TRANSL_FNL_CONT VARCHAR2(500)
+-- );
+
+
